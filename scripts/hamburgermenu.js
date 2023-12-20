99714cd2d44793bfb9873aab24d75b78a57ad98f
Ashley Huang <138391564+ashleyh859@users.noreply.github.com>
2023-12-20 02:24:44 +0000

    fixed nav menu js

--- scripts/hamburgermenu.js
+++ scripts/hamburgermenu.js
@@ -1,18 +1,22 @@
-$("#menubutton").click(function() {
-    if ($("#navmenu").hasClass("hidden")) {
-        $("#navmenu").removeClass("hidden")
+$(document).ready(function () {
+  $("#navmenu").addClass("hidden");
+
+  $("#menubutton").click(function() {
+      if ($("#navmenu").hasClass("hidden")) {
+          $("#navmenu").removeClass("hidden")
+        } else {
+          $("#navmenu").addClass("hidden")
+        }
+    });
+
+
+  $(window).resize(function() {
+      if ($(document).width() < 650) {
+          $("#navmenu").addClass("hidden")
+          $("#menubutton").removeClass("hidden")
       } else {
-        $("#navmenu").addClass("hidden")
+          $("#menubutton").addClass("hidden")
+          $("#navmenu").removeClass("hidden")
       }
-  });
-
-
-$(window).resize(function() {
-    if ($(document).width() < 650) {
-        $("#navmenu").addClass("hidden")
-        $("#menubutton").removeClass("hidden")
-    } else {
-        $("#menubutton").addClass("hidden")
-        $("#navmenu").removeClass("hidden")
-    }
+    });
   });
